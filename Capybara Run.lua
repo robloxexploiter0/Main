f6be7b341fc4c9e6925c68c228eb83dd250c56bf
robloxexploiter0 <robloxexploiter100@gmail.com>
2025-05-28 18:35:30 +0800

    Update Capybara Run.lua

--- Capybara Run.lua
+++ Capybara Run.lua
@@ -40,3 +40,22 @@ local args = {
 
 game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("TrainingMachineService"):WaitForChild("RE"):WaitForChild("StartTraining"):FireServer(unpack(args))
 ]])
+
+AddContent("Toogle", "Auto Train 5", [[local args = {
+    [1] = "Area_1",
+    [2] = "TrainingMachine_1_5",
+    [3] = workspace:WaitForChild("Areas"):WaitForChild("Area_1"):WaitForChild("TrainingMachines"):WaitForChild("TrainingMachine_1_5"):WaitForChild("Location")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("TrainingMachineService"):WaitForChild("RE"):WaitForChild("StartTraining"):FireServer(unpack(args))
+]])
+
+AddContent("Toogle", "Auto Train 6", [[
+local args = {
+    [1] = "Area_1",
+    [2] = "TrainingMachine_1_6",
+    [3] = workspace:WaitForChild("Areas"):WaitForChild("Area_1"):WaitForChild("TrainingMachines"):WaitForChild("TrainingMachine_1_5"):WaitForChild("Location")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("TrainingMachineService"):WaitForChild("RE"):WaitForChild("StartTraining"):FireServer(unpack(args))
+]])
