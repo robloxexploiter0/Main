16eb69cf9c1f5a540f42c6be3a3940059ca5a952
robloxexploiter0 <robloxexploiter100@gmail.com>
2025-05-28 21:51:08 +0800

    Update Capybara Run.lua

--- Capybara Run.lua
+++ Capybara Run.lua
@@ -56,6 +56,7 @@ AddContent("Toogle", "Auto Race", [[local args = {
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("FightService"):WaitForChild("RE"):WaitForChild("FightReady"):FireServer(unpack(args))
+
 local args = {
     [1] = "Area_2",
     [2] = "Area_21748439706_63",
