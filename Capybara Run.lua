3b4656b1164fbb4c067da0c67bd7c5ee6e3cad38
robloxexploiter0 <robloxexploiter100@gmail.com>
2025-05-28 20:11:01 +0800

    Update Capybara Run.lua

--- Capybara Run.lua
+++ Capybara Run.lua
@@ -40,3 +40,14 @@ local args = {
 
 game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("TrainingMachineService"):WaitForChild("RE"):WaitForChild("StartTraining"):FireServer(unpack(args))
 ]])
+
+AddContent("Toogle", "Auto Train 5", [[
+local args = {
+    [1] = "Area_2",
+    [2] = "TrainingMachine_2_5",
+    [3] = workspace:WaitForChild("Areas"):WaitForChild("Area_2"):WaitForChild("TrainingMachines"):WaitForChild("TrainingMachine_2_5"):WaitForChild("Location")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("TrainingMachineService"):WaitForChild("RE"):WaitForChild("StartTraining"):FireServer(unpack(args))
+    
+]])
